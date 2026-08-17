5f3fa528c639fa8030f87317eb3df66c7abe41c7
kozorork <187218994+kozorork@users.noreply.github.com>
2026-08-17 19:35:39 +0200

    fixed a bug in mimetype

--- cairo/mimetype.go
+++ cairo/mimetype.go
@@ -8,5 +8,5 @@ const (
 	MIME_TYPE_JP2       MimeType = "image/jp2"
 	MIME_TYPE_JPEG      MimeType = "image/jpeg"
 	MIME_TYPE_PNG       MimeType = "image/png"
-	MIME_TYPE_URI       MimeType = "image/x-uri"
+	MIME_TYPE_URI       MimeType = "text/x-uri"
 )
